3fdc59132bb02442c4e280de24c51894d6140254
theo cervera lede <theocervera2003@gmail.com>
2025-11-03 01:42:31 -0300

    Implement command parser, ctable, and add 'help'/'clear' commands

--- the0S-Operative-System/kernel.asm
+++ the0S-Operative-System/kernel.asm
@@ -7,6 +7,24 @@ data:
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     mensagemi db 'theOS - Ver 0.1',13,10,'Iniciando...',13,10,0
     sprompt db 13,10,'MY-PC1>',0
+    commands db 'help - list of commands.', 13,10,'clear - clear the screen.',13,10,0
+    error_msg db 'Command not found. Type "help" for a list of commands.',13,10,0
+
+    arg TIMES 64 db 0                                          ; INPUT BUFFER
+
+
+    ;;;;;;;;;;;;;;;;;;;;;;JUMP TABLE;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+    ; Key: 0   1   2  3         4   5   6   7   8         9   10  11
+    ; Letter:  a,  b, c,        d,  e,  f,  g,  h,        i,  j,  k,  l
+    ctable dw ef, ef, clear_fn, ef, ef, ef, ef, help_fn, ef, ef, ef, ef
+    ;
+    ; Key: 12  13  14  15  16  17  18  19  20  21  22  23  24  25
+    ; Letter:  m,  n,  o,  p,  q,  r,  s,  t,  u,  v,  w,  x,  y,  z
+           dw ef, ef, ef, ef, ef, ef, ef, ef, ef, ef, ef, ef, ef, ef
+
 
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;;;;;;;;;;;;;;GLOBAL VARIABLES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -42,21 +60,89 @@ homescreen_fn:
 
 shell_fn:
     mov si, sprompt
-    mov bl, 14                                                 ; yellow
+    mov bl, 14                                                ; yellow
     call print_str_color
+    call cursor
+
+    ; initialize parameters for input
+    xor dx, dx
+    mov di, 0
+    ;ret
+
+    mov bl, 15
+    mov [cor], bl
 
 .input_loop:
     call getc
     call putc
-    cmp al, 13                                                 ; ENTER?
-    jne .input_loop                                            ; if not ENTER keep waiting
-    call exit_to_shell
+    call cursor
+
+
+    cmp dl, 0                                                 ; we already have a char?
+    jne .check_enter                                            ; if not jump back
+    mov dl, al
+    jmp .check_enter
+
+.check_space:
+    cmp al, 32
+    jne .check_arg
+
+    cmp di, 0
+    jne .check_arg
+
+    mov di, arg
+    jmp .check_enter
+
+.check_arg:
+    cmp di, 0
+    je .check_enter
+    stosb
+
+.check_enter:
+    cmp al, 13
+    jne .input_loop
+    cmp di, 0
+    je .find_command
+    mov al, 0
+    stosb
+
+.find_command:
+    mov al, dl
+    sub al, 'a'                                               ; convert to index
+
+    mov ah, 0
+    mov bx ,2
+    mul bx
+
+    mov bx, ctable
+    add ax, bx
+
+    mov bx, [eax]
+    mov [state], bx
     ret
 
 exit_to_shell:
     mov ax, shell_fn
     mov [state], ax
     ret
+
+help_fn:
+    mov si, commands
+    call print_str
+    call exit_to_shell
+    ret
+
+clear_fn:
+    call init_video
+    call exit_to_shell
+    ret
+
+ef:
+    mov si, error_msg
+    call print_str
+    call exit_to_shell
+    ret
+
 
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;;;;;;;;;;;;;;;;;;;;;;;;DRIVERS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -75,6 +161,66 @@ endl:
     mov al, 10
     call putc
     ret
+_put_pixel:
+    mov bp, sp
+    mov ax, 0a000h                                             ; video memory segment
+    push es
+    mov es, ax
+    mov ax, [bp+2]
+    mov cx, 320
+    mul cx
+    add ax, [bp+4]
+    mov bx, ax
+    mov al, [bp+6] ; color
+    es mov [bx], al
+    pop es
+    ret
+
+%macro put_pixel 3
+    pusha
+    push %3; color
+    push %1; x
+    push %2; y
+    call _put_pixel
+    add sp, 6
+    popa
+%endmacro
+
+_draw_horizontal_line:
+    mov bp, sp
+    mov ax, [bp+2]  ; y
+    mov bx, [bp+4]  ; x1 start
+    mov cx, [bp+6]  ; x2 end
+    mov dx, [bp+8]  ; color
+.loop:
+    put_pixel bx, ax, dx
+    inc bx
+    cmp bx, cx
+    jl .loop
+    ret
+
+%macro draw_horizontal_line 4
+    pusha
+    push %4 ; color
+    push %3 ; x2
+    push %2 ; x1
+    push %1 ; y
+    call _draw_horizontal_line
+    add sp, 8
+    popa
+%endmacro
+
+clear_screen: 
+    pusha
+    ; this function clears the screen by drawing black horizontal lines
+    mov bx, 0 ; y = 0
+.loop:
+    draw_horizontal_line bx, 0, 320, 0 ;bx, de x=0 to x=320, color 0 black
+    inc bx
+    cmp bx, 200
+    jl .loop
+    popa
+    ret
 
 init_video:
     mov ah, 00h
@@ -83,9 +229,15 @@ init_video:
     ret
 
     ;;;;;;;;;;;;;;;;;;;;;;TEXT PRINT FUNCTION;;;;;;;;;;;;;;;;;;;
+print_str:
+    mov bl, 7
+    call print_str_color
+    ret
+
 print_char:
     pusha
     mov bl, [cor]
+    mov bh, 0
     mov ah, 0xe
     int 10h
     popa
@@ -102,9 +254,12 @@ cursor:
     popa
     ret
 
-print_str_color:
+
+print_str_prompt:
+
     lodsb
     cmp al, 0
+
     je .done
 
     mov ah,0xe
@@ -113,4 +268,14 @@ print_str_color:
 
 .done:
     call cursor
+    ret
+
+print_str_color:
+    lodsb
+    cmp al, 0
+    je .done
+    mov ah,0xe
+    int 10h
+    jmp print_str_color
+.done:
     ret
